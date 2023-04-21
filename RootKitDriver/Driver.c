18d78b2d87443864d751689630581441eb7fa5e8
Prince 2 lu <Prince 2 lu@DESKTOP>
2023-04-22 01:34:13 +0200

    First .sys added

--- RootKitDriver/Driver.c
+++ RootKitDriver/Driver.c
@@ -1,165 +1,52 @@
-/*++
+#include <ntddk.h>
+#include <wdf.h>
+DRIVER_INITIALIZE DriverEntry;
+EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd;
 
-Module Name:
 
-    driver.c
+NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject,_In_ PUNICODE_STRING RegistryPath){
+    // NTSTATUS variable to record success or failure
+    NTSTATUS status = STATUS_SUCCESS;
 
-Abstract:
-
-    This file contains the driver entry points and callbacks.
-
-Environment:
-
-    Kernel-mode Driver Framework
-
---*/
-
-#include "driver.h"
-#include "driver.tmh"
-
-#ifdef ALLOC_PRAGMA
-#pragma alloc_text (INIT, DriverEntry)
-#pragma alloc_text (PAGE, RootKitDriverEvtDeviceAdd)
-#pragma alloc_text (PAGE, RootKitDriverEvtDriverContextCleanup)
-#endif
-
-NTSTATUS
-DriverEntry(
-    _In_ PDRIVER_OBJECT  DriverObject,
-    _In_ PUNICODE_STRING RegistryPath
-    )
-/*++
-
-Routine Description:
-    DriverEntry initializes the driver and is the first routine called by the
-    system after the driver is loaded. DriverEntry specifies the other entry
-    points in the function driver, such as EvtDevice and DriverUnload.
-
-Parameters Description:
-
-    DriverObject - represents the instance of the function driver that is loaded
-    into memory. DriverEntry must initialize members of DriverObject before it
-    returns to the caller. DriverObject is allocated by the system before the
-    driver is loaded, and it is released by the system after the system unloads
-    the function driver from memory.
-
-    RegistryPath - represents the driver specific path in the Registry.
-    The function driver can use the path to store driver related data between
-    reboots. The path does not store hardware instance specific data.
-
-Return Value:
-
-    STATUS_SUCCESS if successful,
-    STATUS_UNSUCCESSFUL otherwise.
-
---*/
-{
+    // Allocate the driver configuration object
     WDF_DRIVER_CONFIG config;
-    NTSTATUS status;
-    WDF_OBJECT_ATTRIBUTES attributes;
 
-    //
-    // Initialize WPP Tracing
-    //
-    WPP_INIT_TRACING(DriverObject, RegistryPath);
+    // Print "Hello World" for DriverEntry
+    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: DriverEntry\n"));
 
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Entry");
-
-    //
-    // Register a cleanup callback so that we can call WPP_CLEANUP when
-    // the framework driver object is deleted during driver unload.
-    //
-    WDF_OBJECT_ATTRIBUTES_INIT(&attributes);
-    attributes.EvtCleanupCallback = RootKitDriverEvtDriverContextCleanup;
-
+    // Initialize the driver configuration object to register the
+    // entry point for the EvtDeviceAdd callback, KmdfHelloWorldEvtDeviceAdd
     WDF_DRIVER_CONFIG_INIT(&config,
-                           RootKitDriverEvtDeviceAdd
-                           );
+        KmdfHelloWorldEvtDeviceAdd
+    );
 
+    // Finally, create the driver object
     status = WdfDriverCreate(DriverObject,
-                             RegistryPath,
-                             &attributes,
-                             &config,
-                             WDF_NO_HANDLE
-                             );
-
-    if (!NT_SUCCESS(status)) {
-        TraceEvents(TRACE_LEVEL_ERROR, TRACE_DRIVER, "WdfDriverCreate failed %!STATUS!", status);
-        WPP_CLEANUP(DriverObject);
-        return status;
-    }
-
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Exit");
-
+        RegistryPath,
+        WDF_NO_OBJECT_ATTRIBUTES,
+        &config,
+        WDF_NO_HANDLE
+    );
     return status;
 }
 
-NTSTATUS
-RootKitDriverEvtDeviceAdd(
-    _In_    WDFDRIVER       Driver,
-    _Inout_ PWDFDEVICE_INIT DeviceInit
-    )
-/*++
-Routine Description:
-
-    EvtDeviceAdd is called by the framework in response to AddDevice
-    call from the PnP manager. We create and initialize a device object to
-    represent a new instance of the device.
-
-Arguments:
-
-    Driver - Handle to a framework driver object created in DriverEntry
-
-    DeviceInit - Pointer to a framework-allocated WDFDEVICE_INIT structure.
-
-Return Value:
-
-    NTSTATUS
-
---*/
-{
-    NTSTATUS status;
-
+NTSTATUS KmdfHelloWorldEvtDeviceAdd(_In_    WDFDRIVER       Driver, _Inout_ PWDFDEVICE_INIT DeviceInit){
+    // We're not using the driver object,
+    // so we need to mark it as unreferenced
     UNREFERENCED_PARAMETER(Driver);
 
-    PAGED_CODE();
+    NTSTATUS status;
 
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Entry");
+    // Allocate the device object
+    WDFDEVICE hDevice;
 
-    status = RootKitDriverCreateDevice(DeviceInit);
+    // Print "Hello World"
+    KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "KmdfHelloWorld: KmdfHelloWorldEvtDeviceAdd\n"));
 
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Exit");
-
+    // Create the device object
+    status = WdfDeviceCreate(&DeviceInit,
+        WDF_NO_OBJECT_ATTRIBUTES,
+        &hDevice
+    );
     return status;
-}
-
-VOID
-RootKitDriverEvtDriverContextCleanup(
-    _In_ WDFOBJECT DriverObject
-    )
-/*++
-Routine Description:
-
-    Free all the resources allocated in DriverEntry.
-
-Arguments:
-
-    DriverObject - handle to a WDF Driver object.
-
-Return Value:
-
-    VOID.
-
---*/
-{
-    UNREFERENCED_PARAMETER(DriverObject);
-
-    PAGED_CODE();
-
-    TraceEvents(TRACE_LEVEL_INFORMATION, TRACE_DRIVER, "%!FUNC! Entry");
-
-    //
-    // Stop WPP Tracing
-    //
-    WPP_CLEANUP(WdfDriverWdmGetDriverObject((WDFDRIVER)DriverObject));
 }
